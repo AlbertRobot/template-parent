92a8f2f07755a4ea281230dd3ae099524fa85ffa
zhaoxianghui <zhaoxianghui@baidu.com>
2018-06-29 23:28:32 +0800

    init project

---
+++ template-common/src/main/java/com/eden/common/App.java
@@ -0,0 +1,7 @@
+package com.eden.common;
+
+/**
+ * Create by zhaoxianghui on 2018/6/29.
+ */
+public class App {
+}
---
+++ template-common/src/main/java/com/eden/common/Utils/App.java
@@ -0,0 +1,7 @@
+package com.eden.common.Utils;
+
+/**
+ * Create by zhaoxianghui on 2018/6/29.
+ */
+public class App {
+}
